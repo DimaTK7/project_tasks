22726ff23eca29e26262e800d2dc5214a34f27e4
DimaTK7 <mgvk2b9zc51>
2020-08-28 12:20:14 +0300

    dashboard

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,40 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.styles([
+    'resources/asset/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css',
+    'resources/asset/plugins/icheck-bootstrap/icheck-bootstrap.min.css',
+    'resources/asset/plugins/jqvmap/jqvmap.min.css',
+    'resources/asset/plugins/overlayScrollbars/css/OverlayScrollbars.min.css',
+    'resources/asset/plugins/daterangepicker/daterangepicker.css',
+    'resources/asset/plugins/summernote/summernote-bs4.min.css',
+    'resources/asset/dist/css/adminlte.min.css',
+    'resources/asset/plugins/fontawesome-free/css/all.css',
+
+], 'public/css/admin.css');
+
+mix.scripts([
+    'resources/asset/plugins/jquery/jquery.min.js',
+    'resources/asset/plugins/jquery-ui/jquery-ui.min.js',
+    'resources/asset/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/asset/plugins/chart.js/Chart.min.js',
+    'resources/asset/plugins/sparklines/sparkline.js',
+    'resources/asset/plugins/jqvmap/jquery.vmap.min.js',
+    'resources/asset/plugins/jqvmap/maps/jquery.vmap.usa.js',
+    'resources/asset/plugins/jquery-knob/jquery.knob.min.js',
+    'resources/asset/plugins/moment/moment.min.js',
+    'resources/asset/plugins/daterangepicker/daterangepicker.js',
+    'resources/asset/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js',
+    'resources/asset/plugins/summernote/summernote-bs4.min.js',
+    'resources/asset/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js',
+    'resources/asset/plugins/chart.js/Chart.min.js',
+    'resources/asset/plugins/bootstrap/js/bootstrap.bundle.min.js',
+    'resources/asset/dist/js/adminlte.js',
+    'resources/asset/dist/js/demo.js',
+    'resources/asset/dist/js/pages/dashboard.js',
+
+], 'public/js/admin.js');
+
+mix.copy('resources/asset/dist/img', 'public/dist/img');
+mix.copy('resources/asset/plugins/fontawesome-free/webfonts', 'public/webfonts');
+
