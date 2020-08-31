a4b3a73205bf504466eadafd944fadd70a6b67a8
DimaTK7 <mgvk2b9zc51>
2020-08-31 09:46:55 +0300

    query

--- app/Http/Queries/Managerial/TaskQuery.php
+++ app/Http/Queries/Managerial/TaskQuery.php
@@ -20,18 +20,4 @@ class TaskQuery extends AbstractQuery
         ])->paginate(10);
     }
 
-    public function new()
-    {
-        return Task::where('status', 'new')->paginate(10);
-    }
-
-    public function progress()
-    {
-        return Task::where('status', 'progress')->paginate(10);
-    }
-
-    public function done()
-    {
-        return Task::where('status', 'done')->paginate(10);
-    }
 }
